a0f796f2455b482d0a31c7e51691405d0d9ce850
ShabinMuhammedKK <shabinmuhammedkk@gmail.com>
2024-10-16 03:53:29 +0530

    server and folder structre created

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to express application");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server running at http://localhost:${port}`);
+});
